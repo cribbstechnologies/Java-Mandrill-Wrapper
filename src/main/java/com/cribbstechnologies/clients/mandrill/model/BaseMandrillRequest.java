41254a8296cee1a1863bbbdce746208e8257f6f4
Brian Cribbs <briancribbs@Brians-iMac.local>
2013-06-16 17:18:30 -0400

    moving javadoc to a more likely to be read location

--- src/main/java/com/cribbstechnologies/clients/mandrill/model/BaseMandrillRequest.java
+++ src/main/java/com/cribbstechnologies/clients/mandrill/model/BaseMandrillRequest.java
@@ -2,18 +2,20 @@ package com.cribbstechnologies.clients.mandrill.model;
 
 public class BaseMandrillRequest {
 
-	/*
-	 * It's not necessary to set this field manually as the MandrillRESTRequest pulls the property
-	 * from the configuration and populates this object with it on every request.
-	 */
-	private String key;
+    private String key;
 
-	public String getKey() {
-		return key;
-	}
+    public String getKey() {
+        return key;
+    }
 
-	public void setKey(String key) {
-		this.key = key;
-	}
+    /**
+     * It's not necessary to set this field manually as the MandrillRESTRequest pulls the property from the configuration and
+     * populates this object with it on every request
+     * 
+     * @param key
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
 
 }
