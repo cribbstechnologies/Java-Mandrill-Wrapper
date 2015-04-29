30da1ac00ea92e77cedd43958c8000361d182920
Sebastian Beigel <sebastian@beigel.de>
2015-04-29 09:53:20 +0200

    constructors for files and InputStreams added

--- src/main/java/com/cribbstechnologies/clients/mandrill/model/MandrillAttachment.java
+++ src/main/java/com/cribbstechnologies/clients/mandrill/model/MandrillAttachment.java
@@ -1,5 +1,13 @@
 package com.cribbstechnologies.clients.mandrill.model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+
 public class MandrillAttachment {
 
     String type;
@@ -10,6 +18,28 @@ public class MandrillAttachment {
         this.type = type;
         this.name = name;
         this.content = content;
+    }
+
+    public MandrillAttachment(File file, String type) {
+        this.name = file.getName();
+        this.type = type;
+        try {
+            FileInputStream is = new FileInputStream(file);
+            this.content = new String(Base64.encodeBase64(IOUtils.toByteArray(is)));
+            is.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't encode attachment file " + file);
+        }
+    }
+
+    public MandrillAttachment(InputStream is, String fileName, String type) {
+        this.name = fileName;
+        this.type = type;
+        try {
+            this.content = new String(Base64.encodeBase64(IOUtils.toByteArray(is)));
+        } catch (IOException e) {
+            throw new RuntimeException("Can't encode attachment " + fileName);
+        }
     }
 
     public String getContent() {
