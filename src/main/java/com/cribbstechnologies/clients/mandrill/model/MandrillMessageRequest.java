b8431f6ee35eb58026129ab790fa173b883f3927
Aleem Sunderji <aleem@bp-solutions.net>
2016-04-28 17:58:47 -0400

    Remove ineffective async property from MandrillMessageRequest

--- src/main/java/com/cribbstechnologies/clients/mandrill/model/MandrillMessageRequest.java
+++ src/main/java/com/cribbstechnologies/clients/mandrill/model/MandrillMessageRequest.java
@@ -3,7 +3,6 @@ package com.cribbstechnologies.clients.mandrill.model;
 public class MandrillMessageRequest extends BaseMandrillRequest {
 	
 	private MandrillHtmlMessage message;
-        protected Boolean async;
 
 	public MandrillMessage getMessage() {
 		return message;
@@ -13,12 +12,4 @@ public class MandrillMessageRequest extends BaseMandrillRequest {
 		this.message = message;
 	}
 
-        public Boolean getAsync() {
-            return async;
-        }
-
-        public void setAsync(Boolean async) {
-            this.async = async;
-        }
-	
 }
