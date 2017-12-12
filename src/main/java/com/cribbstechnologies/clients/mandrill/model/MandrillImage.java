18709169e8f44b89c4950c92c2bd40fdf50eab09
Martin Islas <mislas87@gmail.com>
2017-12-11 21:08:15 -0600

    add MandrillImage and set to the MandrillMessage.

---
+++ src/main/java/com/cribbstechnologies/clients/mandrill/model/MandrillImage.java
@@ -0,0 +1,44 @@
+package com.cribbstechnologies.clients.mandrill.model;
+
+public class MandrillImage {
+
+    String type;
+    String name;
+    String content;
+
+    public MandrillImage(String type, String name, String content) {
+        this.type = type;
+        this.name = name;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    /**
+     * The attachment is represented by a base-64-encoded string
+     * 
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
